0c03b0c94dc4b99f0e1571155539c0700165e9d0
Scott G. Daniel <scottdaniel25@gmail.com>
2016-11-05 08:31:03 -0700

    setting min contig length to 500, should capture most of genes

--- 01-filter-contigs.sh
+++ 01-filter-contigs.sh
@@ -2,4 +2,4 @@
 
 #This script extracts fasta from anvi-contig.db with contigs greater than 2093
 
-anvi-script-remove-short-contigs-from-fasta -l 2093 -o contigs-longer-than-2093.fa final.contigs.fa
+anvi-script-remove-short-contigs-from-fasta -l 500 -o contigs-longer-than-2093.fa final.contigs.fa
